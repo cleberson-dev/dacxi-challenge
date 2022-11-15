7f43b42398be6ebb47aad66881f76dbc469f2c32
Cleberson F R Junior <cleberson.dev@gmail.com>
2022-11-15 14:54:54 -0300

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Hello, Cleberson!" />
-    </div>
+    <HelloWorld msg="Hello, Cleberson!" />
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
@@ -30,18 +22,12 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
+    justify-content: center;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
     margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
 </style>
