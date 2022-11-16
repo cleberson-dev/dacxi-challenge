553861b0a42981d50be9706a14eca5f56906e2c7
Cleberson F R Junior <cleberson.dev@gmail.com>
2022-11-16 03:45:57 -0300

    Add radio buttons for different coins

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,31 @@ import { getBitcoinPrice, formatToCurrency, getBitcoinHistory } from "./utils";
 
 let bitcoinDate: string;
 let realtimePriceInterval: any;
+let selectedCoin: string;
+
+const supportedCoins = [
+  {
+    id: "bitcoin",
+    symbol: "btc",
+  },
+  {
+    id: 'ethereum',
+    symbol: 'eth', 
+  },
+  {
+    id: "dacxi",
+    symbol: "dacxi",
+  },
+  {
+    id: 'cosmos',
+    symbol: 'atom', 
+  },
+  {
+    id: 'terra-luna-2',
+    symbol: 'luna', 
+  },
+];
+
 const bitcoinPrice = ref(0);
 
 async function storeBitcoinPrice() {
@@ -25,6 +50,8 @@ function startRealtimePriceInterval() {
 function searchHistory() {
   clearRealtimePriceInterval();
 
+  alert(selectedCoin);
+
   const [yyyy, mm, dd] = bitcoinDate.split("-");
   const correctDateString = [dd, mm, yyyy].join("-");
 
@@ -38,6 +65,12 @@ startRealtimePriceInterval();
 
 <template>
   <header>
+    <div class="flex items-center justify-center">
+      <div :key="coin.id" v-for="coin in supportedCoins">
+        <input :value="coin.id" type="radio" name="coin" v-model="selectedCoin" />
+        <label>{{ coin.symbol.toUpperCase() }}</label>
+      </div>
+    </div>
     <h1 class="font-bold underline">Bitcoin</h1>
     <p>Price: {{ formatToCurrency(bitcoinPrice) }}</p>
     <input type="date" v-model="bitcoinDate" />
