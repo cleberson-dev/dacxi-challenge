04e1be2322db26c6ed3fa9ca14fc33d1cdcd272d
Cleberson F R Junior <cleberson.dev@gmail.com>
2022-11-21 01:59:03 -0300

    Refactor CustomButton

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -10,7 +10,7 @@ const emit = defineEmits<{
 }>();
 
 const classes = `
-  disabled:opacity-40 disabled:cursor-default disable
+  disabled:opacity-40 disabled:cursor-default
   font-semibold cursor-pointer mt-2 bg-blue-500 text-black px-4 py-1 rounded
 `;
 </script>
